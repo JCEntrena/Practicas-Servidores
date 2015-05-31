4f8b557492d148926f706a56e64e9a782b865f3f
JCEntrena <jentrena@correo.ugr.es>
2015-05-31 18:09:16 +0200

    Cambios en el benchmark y resultados de la ejecución

--- P4/benchmark.sh
+++ P4/benchmark.sh
@@ -5,12 +5,14 @@
 # Lo utilizaremos para medir tiempos de copia de un archivo de un tamaño considerable a distintos 
 # dispositivos externos, que previamente habremos conectado y montado. Proporcionaremos al script 
 # la ruta del archivo que queremos copiar y las rutas a los diferentes dispositivos, asumiendo que 
-# son todas correctas. Los resultados irán a un archivo .csv para poder operar con ellos. 
+# son todas correctas. Los resultados irán a un archivo .csv para poder operar con ellos. Este archivo 
+# será sobreescrito siempre que ejecutemos el script, por lo que si no queremos que esto suceda deberemos
+# modificarlo para conseguir lo deseado. 
 #######################################################
 
-if [ $# -le 3 ]
+if [ $# -le 4 ]
   then
-    echo "Número de argumentos incorrecto. Uso: RUTA-ARCHIVO, DISP1, DISP2, DISP3"
+    echo "Número de argumentos incorrecto. Uso: RUTA-ARCHIVO, DISP1, DISP2, DISP3, DISP4"
     exit 1
 fi
 
@@ -18,6 +20,7 @@ ARCHIVO=$1
 DISP_1=$2
 DISP_2=$3
 DISP_3=$4
+DISP_4=$5
 
 if [ -e "resultados.csv" ]
 then
@@ -31,7 +34,7 @@ touch resultados.csv
 #######################################################
 # Copia al primer dispositivo. 
 #######################################################
-echo "Copia al primer dispositivo". 
+echo "Copia al primer dispositivo"
 
 START=$(date +%s.%N)
 
@@ -47,7 +50,7 @@ echo $DIFF >> resultados.csv
 ########################################################
 # Copiamos al segundo dispositivo. 
 ########################################################
-echo "Copia al segundo dispositivo". 
+echo "Copia al segundo dispositivo"
 
 START=$(date +%s.%N)
 
@@ -62,7 +65,7 @@ echo $DIFF >> resultados.csv
 #########################################################
 # Copiamos al tercer dispositivo. 
 #########################################################
-echo "Copia al tercer dispositivo". 
+echo "Copia al tercer dispositivo"
 
 START=$(date +%s.%N)
 
@@ -74,4 +77,18 @@ DIFF=$(echo "$END - $START" | bc)
 # Mandamos el resultado al archivo de tiempos. 
 echo $DIFF >> resultados.csv
 
+#######################################################
+# Copia al cuarto dispositivo. 
+#######################################################
+echo "Copia al cuarto dispositivo"
+
+START=$(date +%s.%N)
+
+cp $ARCHIVO $DISP_4 
+
+END=$(date +%s.%N)
+DIFF=$(echo "$END - $START" | bc)
+
+# Mandamos el resultado al archivo de tiempos. 
+echo $DIFF >> resultados.csv
 
